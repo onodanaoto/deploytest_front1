66a5054242b0a52c71c8e774b55d028b5e19569c
Naoto Onoda <onoda@rakudasystem.co.jp>
2024-11-30 03:27:20 +0900

    Add GitHub Actions workflow for Azure deployment

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    BACKEND_URL: process.env.NODE_ENV === 'production'
+      ? 'https://tech0-gen-8-step3-testapp-py2-25.azurewebsites.net'
+      : 'http://localhost:5000'
+  }
+};
+
+export default nextConfig;
